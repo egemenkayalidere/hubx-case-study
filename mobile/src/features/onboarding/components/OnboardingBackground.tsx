21fb94d21b305cdbd287573fa9e2118de3372d6f
Egemen Kayalidere <egemenkayalidere@gmail.com>
2025-12-14 15:54:38 +0300

    feat(onboarding): add background image

---
+++ mobile/src/features/onboarding/components/OnboardingBackground.tsx
@@ -0,0 +1,20 @@
+import { Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const Bg = styled(Image)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+export function OnboardingBackground() {
+  return (
+    <Bg
+      testID="onboarding-background"
+      resizeMode="stretch"
+      source={require('../../../../assets/onboarding/onboarding-background.png')}
+    />
+  );
+}
